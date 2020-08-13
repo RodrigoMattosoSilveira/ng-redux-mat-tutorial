1543a3f9cf741698ee61effb6301ddad66da4bc9
Rodrigo Silveira <r3850365@gmail.com>
2020-08-12 22:10:53 -0700

    install_material  -  feat: Material Installation - Installed material and ancillary libraries: - Angular depends on using NPM, and having the package.lock.json updated! - Wrote the App.Material.Module to contain all Material Modules - Updated the app.module to use the App.Material.Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app.material.module/app.material.module.module';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		AppMaterialModule
+	],
+	providers: [
+		{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
